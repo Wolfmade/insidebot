40e531353ee4506a076b3cb28cb9e3e82b3925ae
Wolfmade <st.moorats@gmail.com>
2015-10-21 14:10:38 +0300

    Updated block section with ClassiCube blocks, made all blocks valid (for now)

--- config.py
+++ config.py
@@ -19,23 +19,23 @@ SILENT_MODE = 0     ## If 1, the bot will only send error messages
 
 ABSOLUTE_SILENT_MODE = 0 ## if 1, the bot will produce no messages whatsoever.
 
-valid_blocks = [1,3,4,5,6,12,13,14,15,16,17,18,19,20,21,22,23,24,
-                             25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                             41,42,44,45,46,47,48,49 ]
-block_names  = {              "stone":1, "dirt":3, "cobblestone":4, "wood":5,
-                              "sappling":6, "tree":6, "sand":12, "gravel":13,
-                              "gold":14, "goldore":14, "iron":15, "ironore":15,
-                              "coal":16, "treetrunk":17, "trunk":17, "leaves":18,
-                              "sponge":19, "glass":20, "red":21, "orange":22,
-                              "yellow":23, "lightgreen":24, "green":25, "aqua":26,
-                              "aquagreen":26, "cyan":27, "blue":28, "purple":29,
-                              "indigo":30, "violet":31, "magenta":32, "pink":33,
-                              "black":34, "grey":35, "white":36, "yellow flower":37,
-                              "rose":38, "redrose":38, "redflower":38,
-                              "red mushroom":39, "brown mushroom":40, "gold block":41,
-                              "ironblock":42, "stair":44, "brick":45, "tnt":46,
-                              "bookcase":47, "mossy cobblestone":48, "green cobblestone":48,
-                              "obsidian":49,"blank":0
+valid_blocks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65 ]
+block_names  = {              "air":0,"blank":0,"nothing":0,"stone":1,"grass":2,"dirt":3,"cobblestone":4,
+                              "wood":5,"planks":5,"plant":6,"sapling":6,"admincrete":7,"opstone":7,"bedrock":7,
+							  "water":8,"stillwater":9,"lava":10,"stilllava":11,"sand":12,"gravel":13,"goldore":14,
+							  "gold ore":14,"gold":14,"ironore":15,"iron ore":15,"iron":15,"coal":16,"log":17,
+							  "trunk":17,"leaves":18,"leaf":18,"sponge":19,"glass":20,"red":21,"orange":22,
+							  "yellow":23,"lime":24,"green":25,"teal":26,"aqua":27,"cyan":28,"blue":29,"indigo":30,
+							  "violet":31,"magenta":32,"pink":33,"black":34,"gray":35,"white":36,"yellowflower":37,
+							  "yellow flower":37,"redflower":38,"red flower":38,"brownmushroom":39,
+							  "brown mushroom":39,"redmushroom":40,"red mushroom":40,"goldblock":41,"ironblock":42,
+							  "doublestair":43,"double stair":43,"stair":44,"brick":45,"tnt":46,"dynamite":46,
+							  "books":47,"bookcase":47,"mossyrocks":48,"mossy rocks":48,"mossystone":48,"mossy stone":48,
+							  "obsidian":49,"cobblestoneslab":50,"cobblestone slab":50,"stoneslab":50,"stone slab":50,
+							  "rope":51,"sandstone":52,"snow":53,"fire":54,"lightpink":55,"light pink":55,"forestgreen":56,
+							  "forest green":56,"brown":57,"deepblue":58,"deep blue":58,"turqoise":59,"ice":60,
+							  "ceramictile":61,"ceramic tile":61,"magma":62,"pillar":63,"crate":64,"stonebrick":65,
+							  "stone brick":65
                 }
 
 ###########################################################
